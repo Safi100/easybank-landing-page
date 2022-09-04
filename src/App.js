5aac926edeb4baee0a4e53f2fedcaa570a7882c2
Safi100 <safinafi12@gmail.com>
2022-09-04 22:34:14 +0300

    finish questions section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import "./app.css"
+import Header from './components/header/Header'
+import Questions from './components/questions/Questions'
 const App = () => {
   return (
     <div>
-      <Navbar />      
+      <Navbar />
+      {/* <Header />   */}
+      <Questions />
     </div>
   )
 }
